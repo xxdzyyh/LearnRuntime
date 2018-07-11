f0f1a9e70ddcc58969aa36a2da930607f640dea9
wangxuefeng <wangxuefeng2@xiaoniu66.com>
2018-07-11 11:14:24 +0800

    Initial Commit

---
+++ LearnRunTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnRunTime
+//
+//  Created by xiaoniu on 2018/7/11.
+//  Copyright © 2018年 com.learn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearnRunTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearnRunTime
+//
+//  Created by xiaoniu on 2018/7/11.
+//  Copyright © 2018年 com.learn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
